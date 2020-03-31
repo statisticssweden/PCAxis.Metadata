bebcfe48132ac50250bb5cdf3ae3c53fc81c42a5
cklinton <60175636+cklinton@users.noreply.github.com>
2020-03-31 14:59:39 +0200

    Restructured files and folders, added unit test projects and updated package properties

---
+++ PCAxis.Metadata.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCAxis.Metadata.UnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
